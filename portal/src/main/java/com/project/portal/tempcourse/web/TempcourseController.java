c6ad53271e0f9144abf89ddd0b9e6b6eb01a61a0
JUHO <JUHO@DESKTOP-2A8GO7K>
2022-08-23 01:46:37 +0900

    tempINsert.html 수정 , TempcourseController 수정

--- portal/src/main/java/com/project/portal/tempcourse/web/TempcourseController.java
+++ portal/src/main/java/com/project/portal/tempcourse/web/TempcourseController.java
@@ -8,8 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.portal.common.Criteria;
 import com.project.portal.professor.service.ProfessorVO;
@@ -107,5 +107,12 @@ public class TempcourseController {
 		return "redirect:tempcourseList";
 	}
 	
+	@PostMapping("/tempweekInsertProc")
+	public String tempweekInsertProc(TempcourseVO vo, TempcourseweekVO voo, Model model) {
+		
+		service.tempweekInsert(voo);
+		
+		return "redirect:tempcourseList";
+	}
 	
 }
