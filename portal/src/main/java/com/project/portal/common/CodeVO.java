3eb2202fe8effb7db27b1e3cbca8bc840ccbcbe0
LEEJUHO123 <jooho6010@naver.com>
2022-08-29 10:09:06 +0900

    CodeVO 넣기

---
+++ portal/src/main/java/com/project/portal/common/CodeVO.java
@@ -0,0 +1,15 @@
+package com.project.portal.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeVO {
+
+	
+	String cd ;
+	String nm ;
+}
