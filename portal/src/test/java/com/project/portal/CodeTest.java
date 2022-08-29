9d92925861d87641db06e371f54e73ad064247b8
Tessa <kwonyuj1996@gmail.com>
2022-08-29 11:07:08 +0900

    yujin commit - code test

---
+++ portal/src/test/java/com/project/portal/CodeTest.java
@@ -0,0 +1,33 @@
+package com.project.portal;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.portal.common.service.CodeVO;
+import com.project.portal.common.service.impl.CodeServiceImpl;
+
+@SpringBootTest
+public class CodeTest {
+	
+	@Autowired 
+	CodeServiceImpl service;
+	
+	// 단건의 그룹코드로 상세 코드 리스트 조회
+	@Test
+	public void codeTest() {
+		List<CodeVO> codeList = service.getDetailList("C01");
+		System.out.println(codeList);
+	}
+	
+	// 여러 건의 그룹코드로 관련 그룹코드의 상세 코드 리스트 전부 조회
+	@Test
+	public void codeListTest() {
+		Map<String, List<CodeVO>> map = service.getAllDetailList("C01", "S01");
+		System.out.println(map.get("C01"));
+		System.out.println(map.get("S01"));
+	}
+}
