e1ba4219a78b48d90cda2a05a4b7dacf84cad90c
Tessa <kwonyuj1996@gmail.com>
2022-09-05 10:45:57 +0900

    yujin commit - logout added

--- portal/src/main/java/com/project/portal/login/service/SecurityConfig.java
+++ portal/src/main/java/com/project/portal/login/service/SecurityConfig.java
@@ -14,9 +14,8 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			// 로그인은 모든 사용자에게 허용 
-			.antMatchers("/login").permitAll()
+		http.authorizeRequests() 
+			.antMatchers("/login").permitAll() // 로그인은 모든 사용자에게 허용
 			// 각각의 사용자의 권한으로 시장하는 매핑 각각의 사용자가 가진 권한에 맞춰서 적용
 			.antMatchers("/student/**").hasAuthority("STUDENT")
 			.antMatchers("/professor/**").hasAuthority("PROFESSOR")
@@ -27,13 +26,16 @@ public class SecurityConfig {
 						XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
 			.and()
 				.formLogin()
-				.loginPage("/login")
-				.successForwardUrl("/login_success_handler")
-				.failureUrl("/access_denied")
+				.loginPage("/login") // 로그인 페이지 지정
+				.successForwardUrl("/login_success_handler") // 로그인 성공 시 포워드되는 URL (세션값 등 지정)
+				//.failureUrl("/access_denied") // 로그인 실패 시 별도로 처리 필요할 경우
 			// CrossSite Request Forgery (disable 해주지 않을 경우 POST 매핑에서 문제 발생)
 			.and()
 				.csrf().disable()
-			
+			.logout()
+				.logoutUrl("/logout") // 로그아웃 요청 URL
+				.logoutSuccessUrl("/login") // 로그아웃이 성공하면 다시 로그인 페이지로 돌아감
+				.invalidateHttpSession(true).deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제 허용
 		;
 		return http.build();
 	}
--- portal/src/main/java/com/project/portal/login/web/UserController.java
+++ portal/src/main/java/com/project/portal/login/web/UserController.java
@@ -1,19 +1,27 @@
 package com.project.portal.login.web;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.portal.login.service.User;
 
 @Controller
 public class UserController {
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/login";
+	}
+	
 	@GetMapping("/login")
 	public String login() {
 		return "login/login";
@@ -32,5 +40,21 @@ public class UserController {
 			return "redirect:/admin";
 		}
 		return "login/userAccess";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request, 
+			HttpServletResponse response,
+			Authentication auth,
+			HttpSession session) {
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			session.removeAttribute("id");
+			if (session.getAttribute("courseCode") != null) {
+				session.removeAttribute("courseCode");
+			}
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login";
 	}
 }
