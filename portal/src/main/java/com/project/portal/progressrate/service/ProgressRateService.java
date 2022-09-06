37a04e08528cfeb08b02939992d9dfa648c6f798
GH PARK <rmsgud135796@naver.com>
2022-09-06 09:22:12 +0900

    gh commit

--- portal/src/main/java/com/project/portal/progressrate/service/ProgressRateService.java
+++ portal/src/main/java/com/project/portal/progressrate/service/ProgressRateService.java
@@ -1,6 +1,19 @@
 package com.project.portal.progressrate.service;
 
+import java.util.List;
 
 public interface ProgressRateService {
-
+	public ProgressRateVO selectWeekAtt();
+	//출석 진행 정보 조회
+	public List<ProgressRateVO> selectAttProgress(ProgressRateVO vo);
+	//강의 진행 정보 조회
+	public List<ProgressRateVO> selectLectureProgress(ProgressRateVO vo);
+	//과제 진행 정보 조회
+	public List<ProgressRateVO> selectReportProgress(ProgressRateVO vo);
+	//중간시험 진행 정보 조회
+	public ProgressRateVO selectMidExamProgress(ProgressRateVO vo);
+	//기말시험 진행 정보 조회
+	public ProgressRateVO selectFinalExamProgress(ProgressRateVO vo);
+	
+	
 }
--- portal/src/main/java/com/project/portal/progressrate/service/ProgressRateVO.java
+++ portal/src/main/java/com/project/portal/progressrate/service/ProgressRateVO.java
@@ -1,5 +1,34 @@
 package com.project.portal.progressrate.service;
 
-public class ProgressRateVO {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class ProgressRateVO {
+	private int studentId; // 학번
+	private String courseCode; // 강의코드
+	private int weekNum; // 주차
+	private int attOption; // 출석여부
+	private String weekCode; // 주차코드
+	private String lectureTitle; // 강의주제
+	private double lecProgressRate; // 강의 진행률
+	private String reportTitle; // 과제명
+	private String reportSubmissionOption; // 제출 여부
+	private String reportCode; // 과제코드
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date reportStartDate; // 과제 시작일
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date reportEndDate; // 과제 시작일
+	private int examScore; // 시험 점수
+	private String examCode; // 시험 코드
+	private int examDuration; // 시험타이머시간
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date examStartDate; // 시험 시작날짜
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date examEndDate; // 시험 종료날짜
+	
+	
 }
