fdf8ccb80a37d9df71cf2b1b10db3429cbd4c603
Jinhyoung Kim <jinhyoung0213@gmail.com>
2022-09-14 11:01:09 +0900

    관리자 정보 조회 수정 추가

--- portal/src/main/java/com/project/portal/admin/service/AdminService.java
+++ portal/src/main/java/com/project/portal/admin/service/AdminService.java
@@ -3,6 +3,9 @@ package com.project.portal.admin.service;
 public interface AdminService {
 	
 	AdminVO adminInfoSelect(AdminVO vo);
-	void updateAdminInfo(AdminVO vo);
+	
+//	void updateAdminInfo(AdminVO vo);
+
+	AdminVO adminInfoUpdate(AdminVO vo);
 
 }
--- portal/src/main/java/com/project/portal/admin/web/AdminController.java
+++ portal/src/main/java/com/project/portal/admin/web/AdminController.java
@@ -1,14 +1,34 @@
 package com.project.portal.admin.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.portal.admin.service.AdminService;
+import com.project.portal.admin.service.AdminVO;
 
 @Controller
 public class AdminController {
 	
-	@Autowired
-	AdminService service;
+	@Autowired AdminService service;
+	
+	// 관리자 본인 정보 조회
+	@RequestMapping("/admin/adminInfo")
+	public String AdminInfo(AdminVO vo, Model model, HttpSession session) {
+		vo.setAdminId((int) session.getAttribute("id"));
+		model.addAttribute("adminInfo", service.adminInfoSelect(vo));
+		return "admin/info/AdminPersonal";
+	}
+	
+	@RequestMapping("/admin/adminInfoUpdate")
+	@ResponseBody
+	public AdminVO adminInfoupdate(AdminVO vo, Model model, HttpSession session) {
+		vo.setAdminId((int) session.getAttribute("id"));
+		return service.adminInfoUpdate(vo);
+	}
 	
 }
